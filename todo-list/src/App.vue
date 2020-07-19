7f0bbfc8dbda5a18d3068be361c39c14f2e89c96
Rafael Passos Guimarães <rapassos@gmail.com>
2020-07-19 13:25:09 -0300

    Edição do modelo principal do header

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -5,6 +5,7 @@
         <h1 class="ui inverted header"> Aplicação Todo List com Vue.js 3</h1>
         <h2 class="ui inverted header">Glaucia Lemos
           <div class="sub header">Cloud Advocate em JavaScript/Node.js @ Microsoft</div>
+          <div class="sub header">Por rapassos durante o Bootcamp DIO de Cloud Computing & Serverless</div>
         </h2>
         <h3 class="ui inverted header">Rio de Janeiro, Brasil - 2020</h3>
         <a class="tiny ui youtube button" type="button" href="https://www.youtube.com/user/l32759">
